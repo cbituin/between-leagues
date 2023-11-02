c4a80e6f4c9bf907fd67a05dc2deca7a7dfdd808
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2023-11-02 14:10:13 -0500

    feat: implements nested routing

--- src/components/app/app-container.tsx
+++ src/components/app/app-container.tsx
@@ -1,10 +1,15 @@
+import { Outlet, Route, Routes } from "react-router-dom";
 import HomePage from "../home-page/home-page";
 
 const AppContainer = () => {
     return (
-        <>
-            <HomePage />            
-        </>
+        <Routes>
+            <Route element={<><h1>Menu Bar</h1><Outlet /></>}>
+                <Route index path="/" element={<HomePage />} />
+                <Route path="/profile" element={<h1>Profile Page</h1>} />
+                
+            </Route>
+        </Routes>
     );
 }
 
